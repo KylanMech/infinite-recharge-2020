1d39348c323df34de89cf569e6633dff248a326e
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-15 13:33:56 -0600

    Add string parse utility

--- InfiniteChargez/src/main/include/Utilities.h
+++ InfiniteChargez/src/main/include/Utilities.h
@@ -4,9 +4,14 @@
 #include "Pair2D.h"
 #include <units/units.h>
 #include <chrono>
+#include <string>
 namespace utilities
 {
     Pair2D<double> squarify(double x, double y);
     units::meter_t simpleAccelToPos(units::meters_per_second_squared_t accel, std::chrono::duration<double> delta);
+
+    template<class T> 
+    T parseSSV(const std::string &str, std::size_t &i);
+    
 }
 #endif
