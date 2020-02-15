44ed44eccbbf62a72d839538a7435122735c37f8
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-15 11:20:59 -0600

    Add file system constants

---
+++ InfiniteChargez/src/main/include/FileConstants.h
@@ -0,0 +1,12 @@
+#ifndef YEET_FILE_CONSTANTS_H
+#define YEET_FILE_CONSTANTS_H
+#include <string>
+namespace utilities
+{
+    namespace filez
+    {
+        const std::string HOME_DIR[]{"/home/lvuser/"};
+        const std::string DEPLOY_DIR[]{"/home/lvuser/deploy/"};
+    }
+}
+#endif
