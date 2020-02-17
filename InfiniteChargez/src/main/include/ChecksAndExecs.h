caa30f680bc8cf83756279203a41a4c12dcd34c3
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-17 13:47:21 -0600

    Add CheckExec interface

---
+++ InfiniteChargez/src/main/include/ChecksAndExecs.h
@@ -0,0 +1,14 @@
+#ifndef YEET_CHECKS_AND_EXECS
+#define YEET_CHECKS_AND_EXECS
+namespace utilities
+{
+    class ChecksAndExecs
+    {
+    public:
+    /**
+     * Check for input and execute actions
+     */
+    virtual void checkAndExec() = 0;
+    };
+}
+#endif
