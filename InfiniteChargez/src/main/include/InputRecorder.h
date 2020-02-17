3fc4d4b1de898dc057b9a175c652ee1a69f809ca
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-17 14:06:03 -0600

    Implement basic recording class

---
+++ InfiniteChargez/src/main/include/InputRecorder.h
@@ -0,0 +1,17 @@
+#ifndef YEET_INPUT_RECORDER_H 
+#define YEET_INPUT_RECORDER_H
+#include "HandlesChecksAndExecs.h"
+
+#include <fstream>
+namespace utilities
+{
+    class InputRecorder
+    {
+    public:
+    InputRecorder() {}
+
+    void designateFile(std::ofstream &recordingFile);
+    void recordFrom(HandlesChecksAndExecs &probablyARobot);
+    };
+}
+#endif
