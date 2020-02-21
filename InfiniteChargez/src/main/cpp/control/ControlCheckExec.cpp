d51275f7dd2607618a9b2ad3fe4add976ff5f805
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-20 20:47:37 -0600

    Comment out unused but possibly useful statements in ControlCheck

--- InfiniteChargez/src/main/cpp/control/ControlCheckExec.cpp
+++ InfiniteChargez/src/main/cpp/control/ControlCheckExec.cpp
@@ -19,17 +19,17 @@
     void Robot::joystickPosition(utilities::XboxInputHandler::joystick_t &&joystickLeft, utilities::XboxInputHandler::joystick_t &&joystickRight)
     {
         //Compilar arguments and preprocessor macros can be passed to remove unused
-        const double JoystickLeftX{leInputHandler.getJoystickLeft().x};
+        //const double JoystickLeftX{leInputHandler.getJoystickLeft().x};
         //Up is negative in Xbox controllers
-        const double JoystickLeftY{-leInputHandler.getJoystickLeft().y};
+        //const double JoystickLeftY{-leInputHandler.getJoystickLeft().y};
 
-        const double JoystickRightX{joystickLeft.x};
+        //const double JoystickRightX{joystickLeft.x};
         //Up isnegative in Xbox controllers
-        const double JoystickRightY{-joystickLeft.y};
+        //const double JoystickRightY{-joystickLeft.y};
         
         //Squarified Values
-        const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
-        const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
+        //const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
+        //const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
 
         leRoboDrive.setMovementMap(leInputHandler.getJoystickLeft());
 
